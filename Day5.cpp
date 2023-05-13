d877d8918f5379d29df56604730215007985615f
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-13 23:52:45 +0530

    Day5

---
+++ Day5.cpp
@@ -0,0 +1,16 @@
+// 189. Rotate Array
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+
+        // for when k is greater than size of array
+        k %= nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
