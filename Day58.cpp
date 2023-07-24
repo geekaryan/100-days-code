baf40cf3bd2e5c5439d8ba50885f109b532f74b0
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-07-24 23:52:18 +0530

    Day58

---
+++ Day58.cpp
@@ -0,0 +1,14 @@
+// 50. Pow(x, n)
+//     Implement pow(x, n),
+//     which calculates x raised to the power n(i.e., xn).
+
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        double ans = pow(x, n);
+
+        return ans;
+    }
+};
