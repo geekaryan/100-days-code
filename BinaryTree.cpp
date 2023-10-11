26536fcbd0b47e3337be6186cdaef462efc4bbb1
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-10-11 18:12:51 +0530

    Binary tree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -84,21 +84,100 @@ void inorder(node *root)
     inorder(root->right);
 }
 
+void preorder(node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+node *buildFromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    cout << "Enter data for root " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1)
+        {
+            temp->left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1)
+        {
+            temp->left = new node(rightData);
+            q.push(temp->left);
+        }
+    }
+}
+
 int main()
 {
 
     node *root = NULL;
 
-    // creating a Tree
-    root = buildTree(root);
-    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-
-    cout << " Printing the level order traversal output " << endl;
-    // level order
+    buildFromLevelOrder(root);
     levelOrderTraversal(root);
 
-    cout << "inorder traversal is: ";
+    // 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
+
+    /*
+    //creating a Tree
+    root = buildTree(root);
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1d
+
+    cout<<" Printing the level order traversal output " <<endl;
+    //level order
+    levelOrderTraversal(root);
+
+
+    cout<<"inorder traversal is: ";
     inorder(root);
+    cout<<endl;
+
+    cout<<"preorder traversal is: ";
+    preorder(root);
+    cout<<endl;
+
+    cout<<"postorder traversal is: ";
+    postorder(root);
+
+    */
 
     return 0;
 }
